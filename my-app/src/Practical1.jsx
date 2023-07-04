a9e693194726604a310e73e45116bc15a6c01262
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-04 18:41:08 +0530

    Update Files

--- my-app/src/Practical1.jsx
+++ my-app/src/Practical1.jsx
@@ -1,8 +1,91 @@
 import React from "react";
 
 const SecondIntro = () => {
-    return (<><h1>This is the Heading 1</h1>
-              <h2>This is the heading 2</h2>
+    return (<>
+<hr/>
+<h1>Practical 1</h1>
+
+     <table border="1"  cellpadding="5px">
+        <thead>
+            <tr>
+                 <th rowspan="3">Day</th> 
+                 <th colspan="3">Seminar</th> 
+            </tr> 
+             <tr>
+                 <th colspan="2">Schedule</th> 
+                 <th rowspan="2">Topic</th> 
+            </tr> 
+             <tr>
+                 <th>Begin</th> 
+                 <th>End</th> 
+            </tr>
+             
+        </thead>
+
+        <tbody>
+             
+            <tr>
+                 <td rowspan="2">Monday</td> 
+                 <td rowspan="2" bgcolor="yellow" >8:00 a.m</td> 
+                 <td rowspan="2" bgcolor="pink" >5:00 p.m</td> 
+                 <td align="center" >Introduction to XML</td> 
+            </tr> 
+             <tr>
+                 <td align="center" >Validity: DTD and Relax NG</td> 
+            </tr> 
+             <tr>
+                 <td rowspan="21">Tuesday</td> 
+                 <td rowspan="8" bgcolor="yellow" >8:00 a.m</td> 
+                 <td rowspan="8" bgcolor="yellow" >11:00 a.m</td> 
+                 <td rowspan="14" align="center" >XPath </td> 
+            </tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr>
+                 <td rowspan="10" bgcolor="yellow" >11:00 a.m</td> 
+                 <td rowspan="10" bgcolor="yellowgreen" >2:00 p.m</td> 
+            </tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr>
+
+                 <td rowspan="7" align="center" >XSL transformation</td> 
+            </tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr>
+                 <td rowspan="3"bgcolor="yellowgreen">2:00 p.m</td> 
+                 <td rowspan="3"bgcolor="pink">5:00 p.m</td> 
+            </tr> 
+             <tr></tr> 
+             <tr></tr> 
+             <tr>
+                 <td>Wednesday</td> 
+                 <td bgcolor="yellow">8:00 a.m</td> 
+                 <td bgcolor="yellowgreen">12:00 p.m</td> 
+                 <td align="center" >XLS Formatting Objects</td> 
+            </tr>
+        </tbody>
+    </table>
+
+    <div class="mt-10">
+    <img src="https://source.unsplash.com/featured/600x501" alt="" />
+    </div>
+    <div class="mt-10">
+      <h2>Image with local "src" not link here, need to confirm with Jay Sir.</h2>
+    <img src="nature.webp" alt="" />
+    </div>
+
+
             </>);
 };
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import FirstIntro from './01_Intro.jsx'
+import FirstIntro from './01_Intro.jsx';
+import Practical from './Practical1.jsx';
 import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (<><FirstIntro/></>);
+root.render (<><FirstIntro/><Practical/></>);
