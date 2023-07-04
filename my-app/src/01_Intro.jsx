cd4ff94b13e148831380846912db2062698a66ed
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-04 15:50:31 +0530

    Update Files

--- my-app/src/01_Intro.jsx
+++ my-app/src/01_Intro.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const FirstIntro = () => {
-    return "This is the first Intro";
+    return (<>
+       <h1>ReactJs</h1>
+    <p>FrontEnd Development tool for creating single page application</p>
+    <p>React, also known as ReactJS, is a well-known front-end JavaScript library for developing user interfaces and related components. It employs the MVC architecture, with a different table of presentation and data availability. One of its distinguishing features is the Virtual DOM and how it handles document access and manipulation. The DOM interacts well with HTML and XML documents, causing them to behave similarly to a tree structure, and each HTML element functions as an object. </p>
+    <p> React, which is powered by Facebook, has been recognized as a fantastic library in the front-end family of tools. It uses the JSX coding style, with a mix of HTML quotes and tag syntax, to create the components. It breaks down larger components into smaller ones that can be managed separately and individually. With this feature, developers' overall productivity is bound to rise. Developers can check out various React Admin Template available in the market to accelerate their development process and create professional and visually appealing admin interfaces. </p>
+
+
+    <h2>Pros: </h2>
+    <ul><li><span data-contrast="auto" lang="EN-IN">Component reusability facilitates collaboration and reuse in other parts of the application.</span><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:259}">&nbsp;</span></li><li><span data-contrast="auto" lang="EN-IN">Through the use of&nbsp;virtual DOM, you can achieve consistent and seamless performance.</span><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:259}">&nbsp;</span></li><li><span data-contrast="auto" lang="EN-IN">It is the ultimate solution to writing components in React hooks because it allows you to write components without classes and makes learning React easier.</span><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:259}">&nbsp;</span></li></ul>
+    
+    <h2>cons</h2>
+    <ul><li><span data-contrast="auto" lang="EN-IN">React dev tools are advanced and extremely useful.</span><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:259}">&nbsp;</span></li><li><span data-contrast="auto" lang="EN-IN">Due to this frontend framework's multiple and constant updates, it is hard to create proper documentation, which has an effect on the learning curve for beginners.</span><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:259}">&nbsp;</span></li><li><span data-contrast="auto" lang="EN-IN">When starting out with the framework, developers find it difficult to understand the complexities of JSX.</span><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:259}">&nbsp;</span></li><li><span data-contrast="auto" lang="EN-IN">It only provides solutions for the front end.</span><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:259}">&nbsp;</span></li></ul>
+    
+    
+    
+    <h1 class="h1">React Version</h1>
+    <p>A complete release history for React is given below. You can also see the full documentation for
+        recent releases on GitHub.</p>
+   
+
+    
+    </>);
 };
 
 export default FirstIntro;
