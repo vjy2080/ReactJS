d8418e3fbc0ca029329d2a86a0582ceb15d5b44b
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-04 12:13:54 +0530

    Update Files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
-
-import ReactDOM from 'react-dom/client';
-
+const Somthing =() => {
+    return "This is first Data"
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render("Data");
+root.render(<Somthing/>);
