e26f7c8fa1b797b1a612137eac514f5aa94cd6e8
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-04 16:37:27 +0530

    Update Files

--- my-app/src/Intro/index.js
+++ my-app/src/Intro/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import FirstIntro from './01_Intro.jsx'
-import Practical1 from './Practical1.jsx'
 import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (<><FirstIntro/><Practical1/></>);
+root.render (<><FirstIntro/></>);
