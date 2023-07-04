10ba492bd3c970abedaa9c4da2ef3dc4cad9ead0
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-04 11:41:26 +0530

    Initial Files

---
+++ my-app/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+
+import ReactDOM from 'react-dom/client';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render("Testing");
