89761bd6147b244377f11f60dbd8e7fa029dee57
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-04 17:00:13 +0530

    Update Files

--- my-app/src/Intro/Practical1/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import JSX_File from './Practical1.jsx'
-import './style.css'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (<><JSX_File/></>);
