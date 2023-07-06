3c9427dbd4d6c79d4ef800bf775c5b2d7ba9eaa4
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-07-06 22:49:52 +0530

    Update Files

--- swift-shop/src/index.js
+++ swift-shop/src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Component/Header.jsx';
 import MainContent from './Component/MainContent.jsx';
+import vjy from "vjy()";
 
 
+const vjy = () => {
+    return (
+        <div>
+         test   
+        </div>
+    );
+};
 
+export default vjy;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (<><Header/><MainContent/></>);
+root.render (<><vjy/><Header/><MainContent/></>);
