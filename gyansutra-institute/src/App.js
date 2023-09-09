d18bfa6f45412a51b36601c0d9dfb0a200bed0d1
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-09-09 11:22:53 +0530

    Init GyanSutra Institute App

---
+++ gyansutra-institute/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Header from './CommonCompo/Header';
+
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
---
+++ gyansutra-institute/src/CommonCompo/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+    MDBNavbar,
+    MDBContainer,
+    MDBIcon,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBCollapse
+} from 'mdb-react-ui-kit';
+
+export default function Header() {
+
+    return (
+        <>
+            <MDBNavbar expand='lg' light style={{ backgroundColor: '#e3f2fd' }}>
+                <MDBContainer fluid>
+                    <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
+                    <MDBNavbarToggler
+                        type='button'
+                        data-target='#navbarColor02'
+                        aria-controls='navbarColor02'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                    >
+                        <MDBIcon icon='bars' fas />
+                    </MDBNavbarToggler>
+                    <MDBCollapse navbar>
+                        <MDBNavbarNav className='me-auto mb-2 mb-lg-0'>
+                            <MDBNavbarItem className='active'>
+                                <MDBNavbarLink aria-current='page' href='#'>
+                                    Home
+                                </MDBNavbarLink>
+                            </MDBNavbarItem>
+                            <MDBNavbarItem>
+                                <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+                            </MDBNavbarItem>
+                            <MDBNavbarItem>
+                                <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+                            </MDBNavbarItem>
+                            <MDBNavbarItem>
+                                <MDBNavbarLink href='#'>About</MDBNavbarLink>
+                            </MDBNavbarItem>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBContainer>
+            </MDBNavbar>
+        </>
+    );
+}
