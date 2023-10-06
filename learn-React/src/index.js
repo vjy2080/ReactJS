c0b67674eee536cb54a0a903b803ce2e99d8e167
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-10-06 12:33:20 +0530

    Update Files

--- learn-React/src/index.js
+++ learn-React/src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-// import App from './App';
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from "./main"
+import { RouterProvider } from "react-router-dom";
+import router from './main'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
