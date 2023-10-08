d5bacb13118554234cb14fa17cc24d81f089d353
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-10-08 22:30:03 +0530

    Update files

--- learn-redux-app/src/pages/AccessStoreData.jsx
+++ learn-redux-app/src/pages/AccessStoreData.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from "./../reducer/users";
 import { Link } from 'react-router-dom';
+
+
 const AccessStoreData = () => {
     const users = useSelector(selectUser);
     console.log("called inside access store data",users);
